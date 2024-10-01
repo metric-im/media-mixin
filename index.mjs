f48627d2328147a6687572f1290580b8ba17c5c2
Michael Sprague <michael@sprague.com>
2024-09-30 19:26:57 -0700

    Uploader module is dropped, code moved to index.mjs

--- index.mjs
+++ index.mjs
@@ -170,6 +170,47 @@ export default class MediaMixin extends Componentry.Module {
         res.status(500).send();
       }
     })
+    router.put("/media/stage/:system?",async (req,res) => {
+      if (!req.account) return res.status(401).send();
+      try {
+        let origin = req.body.origin || "upload"; // alternative is "url"
+        if (!req.body._id) req.body._id = this.connector.idForge.datedId();
+
+        let ext = req.body.type.split('/')[1]
+        let modifier = {
+          $set:{
+            system:req.params.system,
+            origin:origin,
+            status:"staged",
+            _modified:new Date()
+          },
+          $setOnInsert:{
+            _created:new Date()
+          }
+        }
+        if (origin === 'upload') {
+          Object.assign(modifier.$set,{
+            file:req.body._id + '.' + ext,
+            type:req.body.type,
+            size:req.body.size,
+          })
+        } else if (origin === 'url') {
+          Object.assign(modifier.$set,{
+            file:req.body._id + '.png',
+            type:'image/png',
+            url:req.body.url
+          })
+        }
+        if (req.body.captured) modifier.$set.captured = req.body.captured;
+        if (req.account) modifier.$setOnInsert._createdBy = req.account.userId;
+
+        let result = await this.collection.findOneAndUpdate({_id:req.body._id},modifier,{upsert:true});
+        res.json({_id:req.body._id,status:'staged'});
+      } catch (e) {
+        console.error(e);
+        res.status(500).send();
+      }
+    })
 
     router.put('/media/upload/*',async (req,res)=>{
       if (!req.account) return res.status(401).send();
