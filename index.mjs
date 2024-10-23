f14f666a1c59d2a8303bf3020c76b706bf1cd491
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2024-10-23 14:37:46 +0600

    upload, delete, get and rotate images without integrating db

--- index.mjs
+++ index.mjs
@@ -79,24 +79,37 @@ export default class MediaMixin extends Componentry.Module {
         }
         else return this.notFound(req,res)
       } catch (e) {
+        console.log(e)
         res.status(500).send();
       }
     });
 
     router.get('/media/image/rotate/*',async (req,res) => {
       try {
-        await this.storage.rotate(req.params[0]);
+        const rotateDegree = +req.query?.rotateDegree
+        if (!rotateDegree) res.status(400).json({message: 'Set the query param rotateDegree!'})
+
+        await this.storage.rotate(req.params[0], rotateDegree);
         res.status(200).json({});
       } catch (e) {
+        console.log(e)
         res.status(500).json({});
       }
     });
 
     router.delete('/media/image/*', async(req,res) => {
       try {
-        await this.storage.remove(req.params[0]);
-        res.status(200).send();
-      } catch(e) {
+        const id = req.params[0]
+        if (!id) res.status(400).json({'message': 'Image id is required'})
+
+        const isDeleted = await this.storage.remove(req.params[0]);
+        if (isDeleted) {
+          res.status(200).send();
+        } else {
+          res.status(400).json({'message': 'Image was not found or unexpected error'})
+        }
+      } catch (e) {
+        console.log(e)
         res.status(500).send();
       }
     })
@@ -179,68 +192,70 @@ export default class MediaMixin extends Componentry.Module {
       }
     })
 
-    router.put('/media/stage/:system?',async (req, res) => {
-      if (!req.account) return res.status(401).send();
-      try {
-        let origin = req.body.origin || 'upload'; // alternative is 'url'
-        if (!req.body._id) req.body._id = this.connector.idForge.datedId();
-
-        let ext = req.body.type.split('/')[1]
-        let modifier = {
-          $set:{
-            system:req.params.system,
-            origin:origin,
-            status:'staged',
-            _modified:new Date()
-          },
-          $setOnInsert:{
-            _created:new Date()
-          }
-        }
-        if (origin === 'upload') {
-          Object.assign(modifier.$set,{
-            file:req.body._id + '.' + ext,
-            type:req.body.type,
-            size:req.body.size,
-          })
-        } else if (origin === 'url') {
-          Object.assign(modifier.$set,{
-            file:req.body._id + '.png',
-            type:'image/png',
-            url:req.body.url
-          })
-        }
-        if (req.body.captured) modifier.$set.captured = req.body.captured;
-        if (req.account) modifier.$setOnInsert._createdBy = req.account.userId;
-
-        let result = await this.collection.findOneAndUpdate({_id:req.body._id},modifier,{upsert:true});
-        res.json({_id:req.body._id,status:'staged'});
-      } catch (e) {
-        console.error(e);
-        res.status(500).send();
-      }
-    })
+    // router.put('/media/stage/:system?',async (req, res) => {
+    //   if (!req.account) return res.status(401).send();
+    //   try {
+    //     let origin = req.body.origin || 'upload'; // alternative is 'url'
+    //     if (!req.body._id) req.body._id = this.connector.idForge.datedId();
+    //
+    //     console.log(req.body)
+    //
+    //     let ext = req.body.type.split('/')[1]
+    //     let modifier = {
+    //       $set:{
+    //         system:req.params.system,
+    //         origin:origin,
+    //         status:'staged',
+    //         _modified:new Date()
+    //       },
+    //       $setOnInsert:{
+    //         _created:new Date()
+    //       }
+    //     }
+    //     if (origin === 'upload') {
+    //       Object.assign(modifier.$set,{
+    //         file:req.body._id + '.' + ext,
+    //         type:req.body.type,
+    //         size:req.body.size,
+    //       })
+    //     } else if (origin === 'url') {
+    //       Object.assign(modifier.$set,{
+    //         file:req.body._id + '.png',
+    //         type:'image/png',
+    //         url:req.body.url
+    //       })
+    //     }
+    //     if (req.body.captured) modifier.$set.captured = req.body.captured;
+    //     if (req.account) modifier.$setOnInsert._createdBy = req.account.userId;
+    //
+    //     let result = await this.collection.findOneAndUpdate({_id:req.body._id},modifier,{upsert:true});
+    //     res.json({_id:req.body._id,status:'staged'});
+    //   } catch (e) {
+    //     console.error(e);
+    //     res.status(500).send();
+    //   }
+    // })
 
     router.put('/media/upload/*',async (req, res) => {
       if (!req.account) return res.status(401).send();
 
       try {
-        let mediaItem = await this.collection.findOne({_id:req.params[0]},);
-        if (!mediaItem) return res.status(400).send(`${req.params[0]} has not been staged`);
+
         let buffer = req.files.file.data;
         let fileType = req.files.file.mimetype;
-        let file = `${mediaItem._id}.${fileType.split('/')[1]}`;
+        let filePath = req.body.path;
+        const _id = req.body._id
 
         // Normalize images into PNG and capture initial crop spec
         if (fileType.startsWith('image/')) {
           fileType = 'image/png';
-          file = `${mediaItem._id}.png`;
-
-          let spec = await this.storage.getSpec(mediaItem._id, req.query);
-          buffer = await sharp(buffer,{failOnError: false});
+          let spec = await this.storage.getSpec(filePath, req.query);
+          buffer = await sharp(buffer, {failOnError: false});
           buffer = await spec.process(buffer);
+          filePath = spec.path
         }
-        let result = await this.storage.putImage(mediaItem._id,file,fileType,buffer);
+
+        await this.storage.putImage(_id, filePath, fileType, buffer);
         res.json({});
       } catch (e) {
         console.error('/media/upload/* error:', e);
